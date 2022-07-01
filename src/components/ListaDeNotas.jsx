c697377bc033245a44329fa1a4762ec1b4160b18
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-06-30 23:24:26 -0300

    feat:modified file

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import CardNota from "./CardNota";
-
 class LidaDeNotas extends Component {
     render(){
         return(
     <ul>
-        {Array.of("Trablho", "Trablaho", "Estudo").map()}   
-    </ul>
-    );
+        {Array.of("Trablho", "Trablaho", "Estudo").map(categoria  => {
+            return (
+                <li>
+                    <div>{categoria}</div>
+                    <CardNota/>
+                </li>    
+            ) 
+            })}   
+        </ul>
+        );
     }    
 }
 
